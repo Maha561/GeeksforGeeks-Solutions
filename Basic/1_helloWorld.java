8799e7e98a33a617a0cbd53642e37517ba6561e0
Maha561 <mahalakshmianandan04@gmail.com>
2025-02-21 19:35:51 +0530

    added

---
+++ Basic/1_helloWorld.java
@@ -0,0 +1,15 @@
+// Print "Hello World" to the console.
+
+class Solution {
+    static void printHelloWorld() {
+        // Using System.out.println() to print text to the console.
+        System.out.println("Hello World");  // Output: Hello World
+    }
+}
+
+/*
+Time Complexity: O(1) - Runs in constant time as it executes a single statement.
+Space Complexity: O(1) - No extra memory is used apart from output.
+*/
+
+// Problem Link: [Attach GeeksforGeeks Link Here]  
